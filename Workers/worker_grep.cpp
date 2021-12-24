4b43a66e83c316bd3cc4f65cdd168079362f9262
EluciferE <luciferevgeniy@bk.ru>
2021-12-24 21:47:44 +0700

    grep process

--- Workers/worker_grep.cpp
+++ Workers/worker_grep.cpp
@@ -3,7 +3,14 @@
 //
 
 #include "worker_grep.h"
+#include <regex>
 
 std::vector<std::string> WorkerGrep::process(std::vector<std::string> &in, std::string &args) {
-    return std::vector<std::string>();
+    std::vector<std::string> new_data;
+
+    for (auto &line: in){
+        if (line.find(args) != std::string::npos)
+            new_data.insert(new_data.end(), line);
+    }
+    return new_data;
 }
