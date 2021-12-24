f3f1d90e532dd5d879dade20d72856015269dcfa
EluciferE <luciferevgeniy@bk.ru>
2021-12-25 00:30:59 +0700

    Work with args and exceptions

--- main.cpp
+++ main.cpp
@@ -2,13 +2,26 @@
 #include "ConfigReader/config_reader.h"
 #include "WorkExecutor/work_executor.h"
 
-int main() {
-    ConfigReader reader("../config.txt");
-    reader.parseConfig();
-    auto config = reader.getConfig();
-    auto order = reader.getOrder();
+int main(int argv, char** argc) {
+    try {
+        if (argv != 2)
+            throw ExecutorException("Bad number of args: expected 1, but got " + std::to_string(argv - 1));
 
-    WorkerExecutor::Execute(config, order);
+        ConfigReader reader(argc[1]);
+        reader.parseConfig();
+        auto config = reader.getConfig();
+        auto order = reader.getOrder();
+
+        WorkerExecutor::Execute(config, order);
+    }
+    catch (WorkerException& error){
+        std::cout << "Error in worker:\n" + error.what() << std::endl;
+        return 1;
+    }
+    catch (ExecutorException& error){
+        std::cout << "Error in executor:\n" + error.what() << std::endl;
+        return 2;
+    }
 
     return 0;
 }
